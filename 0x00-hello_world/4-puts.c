a9398a924937c137b72cee494be1383f07634552
Lerato Komako <lele.komako@gmail.com>
2023-02-16 04:42:35 -0800

    thanx for no errors betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-*Main -Entry
+*main -Entry
 *Return: Always 0 (Success)
 */
 int main(void)
-{ 
-  puts("\" Programming is like building a multilingual puzzle");
-  return (0);
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
